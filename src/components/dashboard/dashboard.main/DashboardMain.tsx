dd8c5e751fad11a3572b7680d56f7d069dd4af2b
Alexsandro Gomes Clemente <alexsandro.gomes.clemente@hotmail.com>
2024-02-09 03:04:20 -0300

    Tabela pronta

--- src/components/dashboard/dashboard.main/DashboardMain.tsx
+++ src/components/dashboard/dashboard.main/DashboardMain.tsx
@@ -1,13 +1,14 @@
 import "../../../assets/styles/components.css/dashboard.css/Dashboard.component.css";
 import DashboardMainTitle from "./DashboardMainTitle";
 //import DashboardChats from "./charts/DashboardChats";
-import DashboardForm from "./forms/DashboardForm";
+//import DashboardForm from "./forms/DashboardForm";
+import DashboardTable from "./tables/DashboardTable";
 
 function DashboardMain() {
   return (
     <div className="dashboard-main">
       <DashboardMainTitle />
-      <DashboardForm />
+      <DashboardTable />
     </div>
   );
 }
